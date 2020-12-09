e517c6390186dfb9d53263bcc0739c935ba5a723
Udoka Okoye <61575961+udokaokoye@users.noreply.github.com>
2020-12-09 17:31:17 +0100

    Add files via upload

--- All Saints Backend/connection.php
+++ All Saints Backend/connection.php
@@ -1,4 +1,5 @@
 <?php
+define( 'URLROOT', 'http://localhost/All Saints Backend/' );
 header( 'Access-Control-Allow-Origin: *' );
 header( 'Access-Control-Allow-Methods: GET, POST, OPTIONS' );
 header( 'Access-Control-Allow-Headers: *' );
--- All Saints Backend/misc.php
+++ All Saints Backend/misc.php
@@ -4,16 +4,31 @@ include './connection.php';
 if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
     $show_txt = $_POST['show_txt'];
     $banner_txt = $_POST['banner_txt'];
-    $live_img = $_POST['live_img'];
+    $image = $_POST['live_img'];
     $live_txt = $_POST['live_txt'];
     $live_service = $_POST['live_service'];
     $cnt_phone = $_POST['cnt_phone'];
     $cnt_email = $_POST['cnt_email'];
-    $query = "INSERT INTO `random` (`showcase_text`, `banner_txt`, `live_img`, `live_txt`, `live_service`, `cnt_phone`, `cnt_email`) 
+    $live_img_width = $_POST['live_img_width'];
+    if ( isset( $_FILES['live_img'] ) ) {
+        $path = 'Images/';
+        $file_name = $_FILES['live_img']['name'];
+        $file_tmp = $_FILES['live_img']['tmp_name'];
+        $file_type = $_FILES['live_img']['type'];
+        $file = $path . $file_name;
+        if ( move_uploaded_file( $file_tmp, $file ) ) {
+            $error = false;
+            $image = URLROOT . $file;
+        } else {
+            $error = true;
+        }
+    }
+    $query = "INSERT INTO `random` (`showcase_text`, `banner_txt`, `live_img`, `live_img_width`, `live_txt`, `live_service`, `cnt_phone`, `cnt_email`) 
     VALUES ( 
         '".mysqli_real_escape_string( $link, $show_txt )."', 
         '".mysqli_real_escape_string( $link, $banner_txt )."', 
-        '".mysqli_real_escape_string( $link, $live_img )."', 
+        '".mysqli_real_escape_string( $link, $image )."', 
+        '".mysqli_real_escape_string( $link, $live_img_width )."', 
         '".mysqli_real_escape_string( $link, $live_txt )."', 
         '".mysqli_real_escape_string( $link, $live_service )."', 
         '".mysqli_real_escape_string( $link, $cnt_phone )."', 
