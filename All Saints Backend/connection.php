08f82b0dddde01f5e11f7370127879cae3223d30
Udoka Okoye <61575961+udokaokoye@users.noreply.github.com>
2020-12-26 10:10:00 +0100

    Add files via upload

--- All Saints Backend/connection.php
+++ All Saints Backend/connection.php
@@ -1,5 +1,5 @@
 <?php
-define( 'URLROOT', 'http://192.168.1.112/All Saints Backend/' );
+define( 'URLROOT', 'http://192.168.1.4/All Saints Backend/' );
 header( 'Access-Control-Allow-Origin: *' );
 header( 'Access-Control-Allow-Methods: GET, POST, OPTIONS' );
 header( 'Access-Control-Allow-Headers: *' );
